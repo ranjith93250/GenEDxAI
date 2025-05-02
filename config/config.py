beb4f80e41b68b45c96106d9930220d3b5e895d9
Mohan Krishna <142295454+mohan13krishna@users.noreply.github.com>
2025-05-02 14:08:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
-GEMINI_API_KEY = "AIzaSyDllhh8PF8P27IFP6B4EGCjJ87wlhu8rUg"  # Replace with your actual Gemini API key
+GEMINI_API_KEY = "AIzaSyDllhh8PF8P27IFP6--------*******"  # Replace with your actual Gemini API key. I have just added half of my api key due to security reason.
 MONGODB_URI = "mongodb://localhost:27017/"
 DB_NAME = "edu_chatbot"
